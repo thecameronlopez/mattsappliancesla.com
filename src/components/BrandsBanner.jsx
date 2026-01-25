6e76443b55905c9bd780a5fa5edbcad61c4e9e26
thecameronlopez <thecameronlopez@gmail.com>
2026-01-25 14:25:42 -0600

    didnt get too far, need to add some of the css colors from the active page

---
+++ src/components/BrandsBanner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BrandsBanner = () => {
+  return (
+    <div className="brand-banner">
+      <img src="/images/brand-logos/frigidaire.png" alt="Frigidaire Logo" />
+      <img src="/images/brand-logos/ge.png" alt="GE Logo" />
+      <img src="/images/brand-logos/lg.png" alt="LG Logo" />
+      <img src="/images/brand-logos/samsung.png" alt="Samsung Logo" />
+      <img src="/images/brand-logos/whirlpool.png" alt="Whirlpool Logo" />
+      <img src="/images/brand-logos/kenmore.png" alt="Kenmore Logo" />
+    </div>
+  );
+};
+
+export default BrandsBanner;
