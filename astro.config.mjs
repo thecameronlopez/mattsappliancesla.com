ecaa5c27c7cffb5d4c43d52c026be965097a877b
thecameronlopez <kamrin717@gmail.com>
2026-02-02 14:49:23 -0600

    gtags added to rootlayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://mattsappliancesla.com",
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
