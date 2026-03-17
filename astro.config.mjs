23a232aae4ab2db80854ccf3806ece4f73cd1a7b
thecameronlopez <kamrin717@gmail.com>
2026-03-17 10:21:32 -0500

    SEO BABY

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,6 @@ export default defineConfig({
     sitemap({
       filter: (page) =>
         ![
-          "https://mattsappliancesla.com/locations/lafayette/",
           "https://mattsappliancesla.com/services/",
           "https://mattsappliancesla.com/services/delivery/",
           "https://mattsappliancesla.com/services/repairs/",
